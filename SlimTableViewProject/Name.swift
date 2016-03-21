339332ac17db68cf07d7464067722ae208ec5cd6
Maximilian Berndt <sirlunchalot66@web.de>
2016-03-21 21:30:17 +0100

    Add some basic files later used

---
+++ SlimTableViewProject/Name.swift
@@ -0,0 +1,9 @@
+//
+//  Name.swift
+//  SlimTableViewProject
+//
+//  Created by Maximilian Berndt on 21/03/16.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import Foundation
---
+++ SlimTableViewProject/PhoneNumber.swift
@@ -0,0 +1,9 @@
+//
+//  PhoneNumber.swift
+//  SlimTableViewProject
+//
+//  Created by Maximilian Berndt on 21/03/16.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import Foundation

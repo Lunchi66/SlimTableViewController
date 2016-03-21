09fd0d5e507db4a3a80517823f3f358d7252fadc
Maximilian Berndt <sirlunchalot66@web.de>
2016-03-21 16:48:16 +0100

    Initial Commit

---
+++ SlimTableViewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlimTableViewProject
+//
+//  Created by Maximilian Berndt on 21/03/16.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

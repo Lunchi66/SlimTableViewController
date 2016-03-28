41fae37c2addbcfed48849fd776ae4275e13281b
Maximilian Berndt <sirlunchalot66@web.de>
2016-03-28 22:07:32 +0200

    Add Model object, rename Name to Person

---
+++ SlimTableViewProject/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Store.swift
+//  SlimTableViewProject
+//
+//  Created by Maximilian Berndt on 28/03/16.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import Foundation
+
+class Store: NSObject {
+    
+}

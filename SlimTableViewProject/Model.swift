aba7bf5a37743b40e9b0aa944d86386280e0407f
Maximilian Berndt <sirlunchalot66@web.de>
2016-03-28 22:09:12 +0200

    Add Model implementation with few basic methods

--- SlimTableViewProject/Model.swift
+++ SlimTableViewProject/Model.swift
@@ -8,6 +8,44 @@
 
 import Foundation
 
-class Store: NSObject {
+class Model: NSObject {
+    // MARK: - Singleton access
+    
+    /// Singleton model class var to get access to the same model from every class.
+    class var sharedModel: Model {
+        
+        struct Static {
+            static var instance: Model?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = Model()
+        }
+        
+        return Static.instance!
+    }
+    
+    // MARK: - Properties
+    
+    internal var persons: [Person]
+    internal var houses: [House]
+    
+    override init() {
+        persons = []
+        houses = []
+        super.init()
+    }
+    
+    func getNamesForHouse(house:House) -> NSArray {
+        var names: [Person] = []
+        for person in persons{
+            if person.house.name == house.name {
+                print("Person found: \(person.name) for house: \(house.name)")
+                names.append(person)
+            }
+        }
+        return names
+    }
     
 }
